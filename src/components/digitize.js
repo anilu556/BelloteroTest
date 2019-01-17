bf38e7e521459a8883e51a83267766f68107cebe
hmoreno <heberto@kamikazelab.com>
2019-01-16 23:26:34 -0600

    Moved Digitize around a bit so it looks more centered and closer to the design

--- src/components/digitize.js
+++ src/components/digitize.js
@@ -6,30 +6,32 @@ class Digitize extends React.Component {
 	render() {
 		return(
 			<div className='digitize'>
-				<div className="digitize__left">
-					<div className='digitize__left__text'>
-						<div className=' medium'>
-							Digitize your invoices 
-							<div className='light'>
-								and create your own shopping cart.
+				<div className='digitize__container'>
+					<div className="digitize__left">
+						<div className='digitize__left__text'>
+							<div className=' medium'>
+								Digitize your invoices 
+								<div className='light'>
+									and create your own shopping cart.
+								</div>
 							</div>
 						</div>
+						<div className='digitize__left__request'>
+							<form action="">
+								<input type="text" className='digitize__left__request__input regular' 
+									id="fname" name="firstname" 
+									placeholder="Your email address" />
+								<input 
+									className='digitize__left__request__button medium' 
+									type="submit" 
+									value="Request a Demo"
+								/>
+							</form>
+						</div>
 					</div>
-					<div className='digitize__left__request'>
-						<form action="">
-							<input type="text" className='digitize__left__request__input regular' 
-								id="fname" name="firstname" 
-								placeholder="Your email address" />
-							<input 
-								className='digitize__left__request__button medium' 
-								type="submit" 
-								value="Request a Demo"
-							/>
-						</form>
+					<div className='digitize__right'>
+						<img className='digitize__right__img'src={compImg} alt="hero_mockup"></img>
 					</div>
-				</div>
-				<div className='digitize__right'>
-					<img className='digitize__right__img'src={compImg} alt="hero_mockup"></img>
 				</div>
 			</div>
       	);
